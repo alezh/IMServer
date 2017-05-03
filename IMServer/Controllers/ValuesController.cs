c7d11a08ff8a2c14222f379d55e9f60c859607c9
alezh <watman_liu@163.com>
2017-05-03 15:31:25 +0800

    1

---
+++ IMServer/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IMServer.common;
+using Microsoft.Extensions.Options;
+
+namespace IMServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly AppSettings AppSettings;
+        public ValuesController(IOptions<AppSettings> settings)
+        {
+            AppSettings = settings.Value;
+            RedisHelp.REDIS_HOST = AppSettings.REDIS_HOST;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (RedisHelp.StringSet("test","values"))
+            {
+               return RedisHelp.StringGet("test");
+            }
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IMServer/common/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMServer.common
+{
+    public class AppSettings
+    {
+        public string REDIS_HOST { get; set; }
+    }
+}
